18fc4bb5e127e319a93d3a3653356637ae01a268
Luis Gutiérrez <lagm777@gmail.com>
2019-01-18 08:28:49 -0600

    agregando /etc/iiab/

--- install.sh
+++ install.sh
@@ -23,6 +23,11 @@ git clone https://github.com/iiab/iiab-admin-console --depth 1
 git clone https://github.com/iiab/iiab-menu --depth 1
 git clone https://github.com/iiab/iiab-factory --depth 1
 
+mkdir -p /etc/iiab/
+#cd /home/fzt-iiab/
+#cp local_vars.yml /etc/iiab/local_vars.yml
+
+
 cd /opt/iiab/iiab/scripts/
 ./ansible
 # Installs latest Ansible from PPA
