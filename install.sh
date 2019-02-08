e059bba5aabcd3a446e6569d9a5b5839ac7c3cca
Luis Gutiérrez <lagm777@gmail.com>
2019-02-08 17:36:48 -0600

    Reparando la Wiki

--- install.sh
+++ install.sh
@@ -42,7 +42,7 @@ cp /home/fzt-iiab/config.json /library/www/html/js-menu/
 
 rsync -P /mnt/contenido2019/openstreetmap.tar.gz /library/www/html/modules/
 
-rsync -P /mnt/contenido2019/wikipedia.tar.gz /library/zims/content/
+rsync -P /mnt/contenido2019/wikipedia.tar.gz /library/working/zims/
 
 rsync -P /mnt/contenido2019/kalite.tar.gz /library/ka-lite/content/
 
@@ -59,11 +59,26 @@ rm openstreetmap.tar.gz
 #Copia de wikipedia
 #Comentar si no tienes el disco duro
 
-cd /library/zims/content/
+cd /library/working/zims/
 
 tar -zxvf wikipedia.tar.gz
 
 rm wikipedia.tar.gz
+
+chmod 755 /opt/iiab/iiab-admin-console/roles/cmdsrv/files/scripts/*
+
+/opt/iiab/iiab-admin-console/roles/cmdsrv/files/scripts/./zim_install_move.sh gutenberg_es_all_2018-10.zim
+/opt/iiab/iiab-admin-console/roles/cmdsrv/files/scripts/./zim_install_move.sh vikidia_es_all_novid_2018-10.zim
+/opt/iiab/iiab-admin-console/roles/cmdsrv/files/scripts/./zim_install_move.sh wikibooks_es_all_novid_2018-06.zim
+/opt/iiab/iiab-admin-console/roles/cmdsrv/files/scripts/./zim_install_move.sh wikinews_es_all_novid_2018-10.zim
+/opt/iiab/iiab-admin-console/roles/cmdsrv/files/scripts/./zim_install_move.sh wikipedia_es_all_novid_2018-04.zim
+/opt/iiab/iiab-admin-console/roles/cmdsrv/files/scripts/./zim_install_move.sh wikipedia_es_medicine_2018-10.zim
+/opt/iiab/iiab-admin-console/roles/cmdsrv/files/scripts/./zim_install_move.sh wikiquote_es_all_novid_2018-06.zim
+/opt/iiab/iiab-admin-console/roles/cmdsrv/files/scripts/./zim_install_move.sh wikisource_es_all_novid_2018-06.zim
+/opt/iiab/iiab-admin-console/roles/cmdsrv/files/scripts/./zim_install_move.sh wikiversity_es_all_novid_2018-06.zim
+/opt/iiab/iiab-admin-console/roles/cmdsrv/files/scripts/./zim_install_move.sh wikivoyage_es_all_novid_2018-06.zim
+/opt/iiab/iiab-admin-console/roles/cmdsrv/files/scripts/./zim_install_move.sh wiktionary_es_all_novid_2018-10.zim
+/opt/iiab/iiab-admin-console/roles/cmdsrv/files/scripts/./zim_install_move.sh wikiquote_es_all_novid_2018-06.zim
 
 #Copia de ka-lite
 #Comentar si no tienes el disco duro
